ac079564ecdb16ac2a0354632c9c36754a970e48
ostapenko05 <52649321+ostapenko05@users.noreply.github.com>
2019-09-03 16:56:35 +0500

    изменил аккордеон

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
+//////////секция приветствия всплывающее полноэкранное меню
+
 const hoverMenu = document.querySelector('#menu__section-close');
 const hamburgerMenu = document.querySelector('#menu__hamb-act');
 const cross = document.querySelector('#menu__cross');
- 
 
-hamburgerMenu.addEventListener('click',function (e){
+
+hamburgerMenu.addEventListener('click', function (e) {
     e.preventDefault();
 
     hoverMenu.style.display = 'block';
 });
 
-cross.addEventListener('click',function (e){
+cross.addEventListener('click', function (e) {
     e.preventDefault();
 
     hoverMenu.style.display = 'none';
 });
 
+///////////секция команды вертикальный аккордеон
+
+
+////////секция меню горизонтальный аккордеон
+// const menu = document.querySelector('section--menu'),
+//     menuBlock = document.querySelectorAll('menu__block'),
+//     menuBlockLenth = menuBlock.length;
+
+// menu.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     for (let i = 0; i < menuBlockLength; i++) {
+//         menuBlock[i].classList.remove('menu__block--active');
+
+//     }
+// });
+
+// for (let i = 0; i < menuBlockLength; i++) {
+//     e.preventDefault();
+//     menuBlock[i].addEventListener('click', function (e) {
+//         e.stopPropagation();
+//         e.preventDefault();
+
+//         if (menuBlock[i].classList.contains('menu__block--active')) {
+//             menuBlock[i].classList.remove('menu__block--active')
+//         } else {
+//             for (let i = 0; i < menuBlockLength; i++) {
+//                 menuBlock[i].classList.remove('menu__block--active');
+//             }
+//             menuBlock[i].classList.add('menu__block--active')
+//         }
+//     })
+// };
+
+
+var menu = document.querySelectorAll('menu__block');
+
+for (var i = 0; i < menu.length; i++) {
+    
+    menu[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        if (!(this.classList.contains('active'))) {
+            for (var i = 0; i < menu.length; i ++) {
+                menu[i].classList.remove('active');
+            }
+            this.classList.add('active');
+        }
+    })
+}
